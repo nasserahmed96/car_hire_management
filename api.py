74a20462ce725170030986fa3c9b2c725f0ed58e
nasser <abdelnasserahmed96@gmail.com>
2021-09-26 05:19:27 +0200

    Created create customer API

--- api.py
+++ api.py
@@ -21,21 +21,30 @@ class CustomerManager(Resource):
         self.connection = database.get_connection()
         self.cursor = database.get_cursor()
 
-    def insert_new_customer(self):
+    @marshal_with(customer_fields)
+    def post(self):
+        parser = reqparse.RequestParser()
+        parser.add_argument('first_name', type=str, required=True)
+        parser.add_argument('middle_name', type=str, required=False)
+        parser.add_argument('last_name', type=str, required=False)
+        parser.add_argument('email', type=str, required=False)
+        parser.add_argument('phone_number', type=str, required=True)
+        args = parser.parse_args()
         customer = Customer(cursor=self.cursor, connection=self.connection,
-                            first_name="Nasser",
-                            middle_name="Ahmed",
-                            last_name="Ismaiel",
-                            email="abdelnasserahmed@gmail.com",
-                            phone_number="01091238275")
+                            first_name=args['first_name'],
+                            middle_name=args['middle_name'],
+                            last_name=args['last_name'],
+                            email=args['email'],
+                            phone_number=args['phone_number'])
         customer.insert_new_customer()
+        return (customer, status.HTTP_201_CREATED)
 
     @marshal_with(customer_fields)
     def get(self, id):
         customer = Customer(cursor=self.cursor, connection=self.connection)
         customer = customer.get_customer(id)
         if not customer:
-            abort(status.HTTP_404_NOT_FOUND, message="Can'r find the requested Customer")
+            abort(status.HTTP_404_NOT_FOUND, message="Can't find the requested Customer")
         else:
             return customer
 
@@ -44,7 +53,7 @@ customer_manager = CustomerManager()
 def run_flask_app():
     app = Flask(__name__)
     api = Api(app)
-    api.add_resource(CustomerManager, '/api/get_customer/<int:id>')
+    api.add_resource(CustomerManager, '/api/customers')
     app.run()
 
 if __name__ == "__main__":
